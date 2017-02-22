e777c3df368ab9b6e6127822b5c81ce6f832a332
yuanpeng <Hello World@Empire>
2017-02-22 16:08:33 +0800

    master:另外一个提交测试

--- src/test/java/test/MySFTP.java
+++ src/test/java/test/MySFTP.java
@@ -28,63 +28,63 @@ public class MySFTP {
 * @param password 密码
 * @return
 */
-public ChannelSftp connect(String host, int port, String username,
-String password) {
-ChannelSftp sftp = null;
-try {
-JSch jsch = new JSch();
-jsch.getSession(username, host, port);
-Session sshSession = jsch.getSession(username, host, port);
-System.out.println("Session created.");
-sshSession.setPassword(password);
-Properties sshConfig = new Properties();
-sshConfig.put("StrictHostKeyChecking", "no");
-sshSession.setConfig(sshConfig);
-sshSession.connect();
-System.out.println("Session connected.");
-System.out.println("Opening Channel.");
-Channel channel = sshSession.openChannel("sftp");
-channel.connect();
-sftp = (ChannelSftp) channel;
-System.out.println("Connected to " + host + ".");
-} catch (Exception e) {
-
-}
-return sftp;
-}
-
-/**
-* 上传文件
-* @param directory 上传的目录
-* @param uploadFile 要上传的文件
-* @param sftp
-*/
-public void upload(String directory, String uploadFile, ChannelSftp sftp) {
-try {
-sftp.cd(directory);
-File file=new File(uploadFile);
-sftp.put(new FileInputStream(file), file.getName());
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
-
-/**
-* 下载文件
-* @param directory 下载目录
-* @param downloadFile 下载的文件
-* @param saveFile 存在本地的路径
-* @param sftp
-*/
-public void download(String directory, String downloadFile,String saveFile, ChannelSftp sftp) {
-try {
-sftp.cd(directory);
-File file=new File(saveFile);
-sftp.get(downloadFile, new FileOutputStream(file));
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+//public ChannelSftp connect(String host, int port, String username,
+//String password) {
+//ChannelSftp sftp = null;
+//try {
+//JSch jsch = new JSch();
+//jsch.getSession(username, host, port);
+//Session sshSession = jsch.getSession(username, host, port);
+//System.out.println("Session created.");
+//sshSession.setPassword(password);
+//Properties sshConfig = new Properties();
+//sshConfig.put("StrictHostKeyChecking", "no");
+//sshSession.setConfig(sshConfig);
+//sshSession.connect();
+//System.out.println("Session connected.");
+//System.out.println("Opening Channel.");
+//Channel channel = sshSession.openChannel("sftp");
+//channel.connect();
+//sftp = (ChannelSftp) channel;
+//System.out.println("Connected to " + host + ".");
+//} catch (Exception e) {
+//
+//}
+//return sftp;
+//}
+//
+///**
+//* 上传文件
+//* @param directory 上传的目录
+//* @param uploadFile 要上传的文件
+//* @param sftp
+//*/
+//public void upload(String directory, String uploadFile, ChannelSftp sftp) {
+//try {
+//sftp.cd(directory);
+//File file=new File(uploadFile);
+//sftp.put(new FileInputStream(file), file.getName());
+//} catch (Exception e) {
+//e.printStackTrace();
+//}
+//}
+//
+///**
+//* 下载文件
+//* @param directory 下载目录
+//* @param downloadFile 下载的文件
+//* @param saveFile 存在本地的路径
+//* @param sftp
+//*/
+//public void download(String directory, String downloadFile,String saveFile, ChannelSftp sftp) {
+//try {
+//sftp.cd(directory);
+//File file=new File(saveFile);
+//sftp.get(downloadFile, new FileOutputStream(file));
+//} catch (Exception e) {
+//e.printStackTrace();
+//}
+//}
 
 /**
 * 删除文件
